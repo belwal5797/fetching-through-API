29e03afa462ee45a5b573c6fb6e0dd18b2a3f880
KARAN BELWAL <70758114+belwal5797@users.noreply.github.com>
2021-06-12 12:07:42 +0530

    Update ListOfUsers1.jsx

--- ListOfUsers1.jsx
+++ ListOfUsers1.jsx
@@ -3,7 +3,6 @@ import React,{useEffect, useState}from 'react';
 const ListOfUsers1=()=>{
 
     const [user,setUser]=useState([]);
-    const [newUser,SetnewUser]=useState([]);
 
     const getUsers=async()=>{
 
